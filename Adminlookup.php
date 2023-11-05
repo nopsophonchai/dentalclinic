15751a019e25b1c18e8449aa2dea04b5b3de8946
Messnaldo <144195309+Messnaldo@users.noreply.github.com>
2023-11-05 23:07:13 +0700

    Create Adminlookup.php

---
+++ Adminlookup.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Dentiste </title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="logo-containersignup">
+        </div>
+        <div class="signup-form">
+            <h2 class="signup-heading"> Account Lookup </h2>
+
+            <form action="dentalIndex.php" method="post">
+                    
+                    <div class="form-group">
+                        
+                        <input type="text" id="search" name="SearchText" >
+                  
+                    </div>
+                    <input type="submit" name="searchbutton" value="Search">
+                    
+            </form>
+        </div>
+    </div>
+</body>
+</html>
