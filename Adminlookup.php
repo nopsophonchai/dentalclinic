4a0fbb3ba07eafbb867a4ec5167e4e2c9f7d29f1
Messnaldo <144195309+Messnaldo@users.noreply.github.com>
2023-11-05 23:35:16 +0700

    Update Adminlookup.php

--- Adminlookup.php
+++ Adminlookup.php
@@ -11,7 +11,57 @@
         <div class="signup-form">
             <h2 class="signup-heading"> Account Lookup </h2>
 
-            <form action="dentalIndex.php" method="post">
+            <form action="<!DOCTYPE html>
+<html>
+<head>
+    <title> Dentiste </title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="logo-containersignup">
+        </div>
+        <div class="signup-form">
+            <h2 class="signup-heading"> Account Lookup </h2>
+
+            <form action="<!DOCTYPE html>
+<html>
+<head>
+    <title> Dentiste </title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="logo-containersignup">
+        </div>
+        <div class="signup-form">
+            <h2 class="signup-heading"> Account Lookup </h2>
+
+            <form action="Adminmanager.php" method="post">
+                    <div class="form-group">
+                        
+                        <input type="text" id="SearchText" name="SearchText" >
+                  
+                    </div>
+                    <input type="submit" name="searchbutton" value="Search">
+                    <input type="submit" name="backbutton" value="Back">
+            </form>
+        </div>
+    </div>
+</body>
+</html>" method="post">
+                    <div class="form-group">
+                        
+                        <input type="text" id="SearchText" name="SearchText" >
+                  
+                    </div>
+                    <input type="submit" name="searchbutton" value="Search">
+                    <input type="submit" name="backbutton" value="Back">
+            </form>
+        </div>
+    </div>
+</body>
+</html>p" method="post">
                     
                     <div class="form-group">
                         
