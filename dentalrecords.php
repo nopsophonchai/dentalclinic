27a9e70f4f24374b545395ce797a76e0b130b46f
Amal Topa <amaltopastudent@gmail.com>
2023-11-06 16:52:21 +0700

    new stuff

--- dentalrecords.php
+++ dentalrecords.php
@@ -10,15 +10,19 @@
         </div>
         <div class="signup-form">
             <h2 class="signup-heading"> My Appointments </h2>
+            <div class="app-form">
             <table> 
                 <col width="20%">
-                <col width="30%">
-                <col width="30%">
+                <col width="20%">
+                <col width="20%">
+                <col width="20%">
+
                
                 <tr> 
-                    <th>Date</th>
-                    <th>Doctor</th>
-                    <th>Reason</th>
+                    <th>Time</th>
+                    <th>Diagnosis</th>
+                    <th>Treatment</th>
+                    <th>Note</th>
                 </tr>
             </table>
             <!--
@@ -41,7 +45,7 @@
                 -->
             <div class="form-groupmyapp">
                         <button type="submit" name="exit" >Exit</button>
-            </div>
+                    </div>
             </div>
         </div>
     </div>
