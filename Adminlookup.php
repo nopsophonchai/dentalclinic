a42e834e253f23a8a3e96822d2f7bfe1d28fd1f7
Messnaldo <144195309+Messnaldo@users.noreply.github.com>
2023-11-12 17:24:40 +0700

    Update Adminlookup.php

--- Adminlookup.php
+++ Adminlookup.php
@@ -11,15 +11,19 @@
         <div class="signup-form">
             <h2 class="signup-heading"> Account Lookup </h2>
 
-            <form action="Adminmanager.php" method="post">
+            <form action="Adminlookupresult.php" method="post">
                     <div class="form-group">
                         
                         <input type="text" id="SearchText" name="SearchText" >
                   
                     </div>
                     <input type="submit" name="searchbutton" value="Search">
-                    <input type="submit" name="backbutton" value="Back">
-            </form>
+                    </form>
+                    <form action="Adminmanager.php" method="post">
+                <input type="submit" name="backbutton" value="Back"  >
+</form>
+                
+            
         </div>
     </div>
 </body>
